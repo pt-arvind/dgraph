dac649fb809f4a05169538b75c2dcbc439ac71cd
Arvind Subramanian <pt.arvind@gmail.com>
2024-10-08 08:55:18 -0400

    vendoring 24.0.3

---
+++ vendor/github.com/dgraph-io/gqlparser/v2/validator/validator.go
@@ -0,0 +1,57 @@
+package validator
+
+import (
+	"sort"
+
+	. "github.com/dgraph-io/gqlparser/v2/ast"
+	"github.com/dgraph-io/gqlparser/v2/gqlerror"
+)
+
+type AddErrFunc func(options ...ErrorOption)
+
+type ruleFunc func(observers *Events, addError AddErrFunc)
+
+type rule struct {
+	name string
+	// rules will be called in the ascending order
+	order int
+	rule  ruleFunc
+}
+
+var rules []rule
+
+// addRule to rule set.
+// f is called once each time `Validate` is executed.
+func AddRule(name string, f ruleFunc) {
+	rules = append(rules, rule{name: name, rule: f})
+}
+
+// AddRuleWithOrder to rule set with an order.
+// f is called once each time `Validate` is executed.
+func AddRuleWithOrder(name string, order int, f ruleFunc) {
+	rules = append(rules, rule{name: name, order: order, rule: f})
+	sort.Slice(rules, func(i, j int) bool {
+		return rules[i].order < rules[j].order
+	})
+}
+
+func Validate(schema *Schema, doc *QueryDocument, variables map[string]interface{}) gqlerror.List {
+	var errs gqlerror.List
+
+	observers := &Events{}
+	for i := range rules {
+		rule := rules[i]
+		rule.rule(observers, func(options ...ErrorOption) {
+			err := &gqlerror.Error{
+				Rule: rule.name,
+			}
+			for _, o := range options {
+				o(err)
+			}
+			errs = append(errs, err)
+		})
+	}
+
+	Walk(schema, doc, observers, variables)
+	return errs
+}
